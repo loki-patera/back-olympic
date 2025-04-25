e21845547082df049b7ba8e2b76ffb3c41709d52
loki-patera <boulet.g@outlook.be>
2025-04-25 11:03:38 +0200

    Create user and person model + Create superuser and admin

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,7 @@ DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [os.environ.get("DJANGO_ALLOWED_HOSTS")]
 
+AUTH_USER_MODEL = "user.User"
 
 # Application definition
 
@@ -41,6 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "user"
 ]
 
 MIDDLEWARE = [
